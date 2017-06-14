dd74f1ae6d2e3ef3c8dbe4c73d4dc64b290a12c4
nguyenduy <nguyenduy@NGUYENDUY-UBUNTU>
2017-06-14 23:50:08 +0700

    Add folder processing operation

--- src/main/scala/SeparateDocs.scala
+++ src/main/scala/SeparateDocs.scala
@@ -9,14 +9,35 @@ import java.io.FileWriter
 object SeparateDocs {
   def main(args: Array[String]): Unit = {
     val fileIn = new File(args(0)) // args(0) = ./trndocs.dat
-    val reader = new BufferedReader(new FileReader(fileIn))
+    var reader: BufferedReader = null
+    if (fileIn.isDirectory()) {
+      val listFiles = fileIn.listFiles()
+      var startFilename = 0
+      listFiles.foreach(file => {
+        reader = new BufferedReader(new FileReader(file))
+        startFilename = writeFile(reader, startFilename, args(1))
+      })
+    } else {
+      reader = new BufferedReader(new FileReader(fileIn))
+      writeFile(reader, 0, args(1))
+    }
 
-    var name = 0
+  }
+
+  /**
+   * Write separated text files
+   * @param reader buffer reader of a file
+   * @param startName start number of file name
+   * @param outputDir output directory
+   * @return number of last file name
+   */
+  def writeFile(reader: BufferedReader, startName: Int, outputDir: String): Int = {
+    var name = startName
     reader.readLine() // skip 01 line
     var line = reader.readLine()
     while (line != null) {
       println("Writing file " + name + "...")
-      val fileOut = new File(args(1) + File.separator + zeroPad(name, 6)) // args(1) = ./output
+      val fileOut = new File(outputDir + File.separator + zeroPad(name, 6)) // args(1) = ./output
       val writer = new BufferedWriter(new FileWriter(fileOut))
       writer.flush
 
@@ -29,7 +50,8 @@ object SeparateDocs {
       name += 1
       line = reader.readLine()
     }
-
+    reader.close()
+    name
   }
 
   def zeroPad(number: Int, width: Int): String = {
